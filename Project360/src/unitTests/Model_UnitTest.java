1b548c40bd44213728d514365944484963027d5f
alan8r-uwt <alan8r@uw.edu>
2022-06-04 19:13:31 -0700

    added some VERY basic tests

--- Project360/src/unitTests/Model_UnitTest.java
+++ Project360/src/unitTests/Model_UnitTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import model.AppFile;
+
 class Model_UnitTest {
 
 	@BeforeAll
@@ -38,5 +40,18 @@ class Model_UnitTest {
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-
+	
+	
+	@Test
+	void testAppFile() {
+		AppFile appFile = new AppFile("~/test.txt");
+		assertNotNull(appFile);
+		appFile.updateFileInfo();
+		assertNotNull(appFile.getFilePath());
+		assertNotNull(appFile.getFileName());
+		assertNotNull(appFile.getFileExtension());
+		assertNotNull(appFile.getFileTypeDescription());
+		assertNotNull(appFile.getFileIcon());
+		assertNotNull(appFile.getLabelsArray());
+	}
 }
